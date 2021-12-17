ed7990790c845028ccdc5bf2ec446a9a7686cc95
Egor <kevok_0011@mail.ru>
2021-12-17 23:31:16 +0300

    upd v2.0
    new

--- JS/comments.js
+++ JS/comments.js
@@ -1,19 +1,26 @@
-$(document).ready( function () {
-    $(document).on("click","#add_comment",function () {
-        var form = $("form[name='form']");
-        form.css("display","block");
-        $("#add_comment").replaceWith(form);
+let comments = [];
+if (localStorage.getItem('comments')) {
+    comments = JSON.parse(localStorage.getItem('comments'))
+}
+showComments();
+
+document.getElementById('comment-add').addEventListener('click', function(event){
+    let commentName = document.getElementById('comment-name');
+
+
+    let comment = {
+        Text : commentName.value,
+    }
+    comments.push(comment);
+    localStorage.setItem('comments', JSON.stringify(comments));
+    showComments();
+})
+
+function showComments (){
+    let commentField = document.getElementById('comments');
+    let out = '';
+    comments.forEach(function(item){
+        out += `<p class="block1">${item.Text}</p>`;
     });
-
-    $(document).on("click", "input[name='send']", function () {
-        var comment = $("textarea[name='comment']").val ();
-        $("<p class='block1'>" + comment + "</p>").appendTo("div#comments");
-    });
-});
-
-
-
-
-
-
-
+    commentField.innerHTML = out;
+}
