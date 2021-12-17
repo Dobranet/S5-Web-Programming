e61d04df17da3c5987673a69f2a03fe47b057e50
Egor <kevok_0011@mail.ru>
2021-12-17 16:09:11 +0300

    L1-6 update
    update

---
+++ JS/comments.js
@@ -0,0 +1,19 @@
+$(document).ready( function () {
+    $(document).on("click","#add_comment",function () {
+        var form = $("form[name='form']");
+        form.css("display","block");
+        $("#add_comment").replaceWith(form);
+    });
+
+    $(document).on("click", "input[name='send']", function () {
+        var comment = $("textarea[name='comment']").val ();
+        $("<p class='block1'>" + comment + "</p>").appendTo("div#comments");
+    });
+});
+
+
+
+
+
+
+
